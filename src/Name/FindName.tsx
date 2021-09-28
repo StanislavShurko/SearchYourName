0897bba9dc02dc020f3188ba58344f40a6a72b45
Stanislav Shurko <stas.shurko@gmail.com>
2021-09-28 12:23:27 +0300

    NameFound

---
+++ src/Name/FindName.tsx
@@ -0,0 +1,47 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { NameInput } from "./NameInput";
+
+export function FindName() {
+
+    const [person, setPerson] = useState("")
+    const [firstCountry, setFirstCountry] = useState("")
+    const [secondCountry, setSecondCountry] = useState("")
+    const [thirdCountry, setThirdCountry] = useState("")
+
+    function handlePerson(e: string) {
+        setPerson(e)
+    }
+
+    async function getLatest() {
+        try{
+            const res = await axios.get(`https://api.nationalize.io?name=${person}`);
+            setFirstCountry("1." + res.data.country[0].country_id)
+            setSecondCountry("2." + res.data.country[1].country_id)
+            setThirdCountry("3." + res.data.country[2].country_id)
+        }catch{
+            console.log(Error)
+        }
+    }
+
+    useEffect(() => {
+            void getLatest();
+        }
+        , [person]
+    )
+
+    return (
+        <div className="find_name">
+            <div className="name_input">
+                <NameInput
+                    inputValue={handlePerson}
+                />
+            </div>
+            <div className="column">
+                <div className="first">{secondCountry}</div>
+                <div className="second">{firstCountry}</div>
+                <div className="third">{thirdCountry}</div>
+            </div>
+        </div>
+    )
+}
---
+++ src/Name/NameInput.tsx
@@ -0,0 +1,13 @@
+
+
+interface NameInputProps {
+    inputValue: (e: any) => void
+}
+
+export function NameInput({inputValue}: NameInputProps) {
+    return (
+        <div>
+            <input type="text" onChange={ e => inputValue(e.target.value)} />
+        </div>
+    )
+}
