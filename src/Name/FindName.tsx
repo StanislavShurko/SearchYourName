05c79d8201ff59c70153d8231c25729df3b868b3
Stanislav Shurko <stas.shurko@gmail.com>
2021-10-06 20:42:11 +0300

    Name0.3

--- src/Name/FindName.tsx
+++ src/Name/FindName.tsx
@@ -1,11 +1,13 @@
 import axios from "axios";
 import React, {useEffect, useState} from "react";
 import {NameInput} from "./NameInput";
+import { RequestState } from "./RequestState";
 
 export function FindName() {
 
     const [person, setPerson] = useState("")
     const [country, setCountry] = useState("")
+    const [rs, setRs] = useState<RequestState>(RequestState.none);
 
     function handlePerson(e: string) {
         setPerson(e)
@@ -13,16 +15,19 @@ export function FindName() {
 
     async function getLatest() {
         try {
+            setRs(RequestState.request)
             const res = await axios.get<any>(`https://api.nationalize.io?name=${person}`);
             setCountry(res.data?.country)
+            setRs(RequestState.success)
         } catch (e) {
+            setRs(RequestState.failure)
             console.log(e)
         }
     }
 
     useEffect(() => {
             void getLatest();
-            console.log(country[0]["country_id"])
+            console.log(country[0])
         }
         , [person]
     )
@@ -35,9 +40,7 @@ export function FindName() {
                 />
             </div>
             <div className="column">
-               <div className="first"></div>
-               <div className="second"></div>
-               <div className="third"></div>
+               <div className="first">{}</div>
             </div>
         </div>
     )
---
+++ src/Name/RequestState.ts
@@ -0,0 +1,6 @@
+export enum RequestState {
+    none,
+    request,
+    success,
+    failure
+}
